f5934aba7a8124a0e236683fa7178a37a4653a2e
israelcortesflores <israelcortesflores@gmail.com>
2021-05-28 22:47:59 +0200

    Refactorizamos Metodo RESTA(Test OK)

--- Calculadora/CalculadoraLib.cs
+++ Calculadora/CalculadoraLib.cs
@@ -15,9 +15,12 @@ namespace Calculadora
             return num1 + num2;
         }
 
-        public decimal Resta(decimal parOperando1, decimal parOperando2)
+        public T Resta<T>(T parOperando1, T parOperando2)
         {
-            return parOperando1 - parOperando2;
+            dynamic num1 = parOperando1;
+            dynamic num2 = parOperando2;
+
+            return num1 - num2;
         }
 
     }
