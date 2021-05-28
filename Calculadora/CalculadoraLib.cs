077991bac11d22cc77495289ee05290075f2c313
israelcortesflores <israelcortesflores@gmail.com>
2021-05-28 22:43:29 +0200

    Add Class CalculadoraLib y Metodo SUMA (Test OK)

---
+++ Calculadora/CalculadoraLib.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    public class CalculadoraLib
+    {
+        public decimal Suma(decimal parOperando1, decimal parOperando2)
+        {
+            return parOperando1 + parOperando2;
+        }
+    }
+}
